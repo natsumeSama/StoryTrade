8011aa3b4baf6e4d7270f58da3b7f3bf25cae9de
Yudas <84617860+natsumeSama@users.noreply.github.com>
2024-04-23 22:18:07 +0200

    tentative d'intégrer camera picture plugin
    ne marche pas

--- www/js/livreform.js
+++ www/js/livreform.js
@@ -1,7 +1,13 @@
 const form = document.getElementById("form");
+const Cover= document.getElementById("Cover");
+const Title= document.getElementById("Title");
+const Author= document.getElementById("Author");
+const Editor= document.getElementById("Editor");
+const Description= document.getElementById("Description");
 
     // L'application s'éxecute dans un navigateur
     const apiUrl = 'http://localhost:3000/livres';
+
 
 form.addEventListener('click', function(e){
     e.preventDefault();
@@ -12,7 +18,10 @@ form.addEventListener('click', function(e){
     const description = document.getElementById('description').value;
 
     const data = { title, author, editor, description};
-
+    Title.textContent = title;
+    Author.textContent += author;
+    Editor.textContent += editor;
+    Description.textContent = description;
     fetch(apiUrl,{
         method: "POST",
         headers: {
@@ -31,10 +40,11 @@ document.getElementById('cameraTakePicture').addEventListener('click', function(
         destinationType: Camera.DestinationType.FILE_URI,
         correctOrientation: true
     });
+    Title.textContent="echec";
 });
 
 function onSuccess(imageUri) {
-    var image = document.getElementById('myImage');
+    var image = document.getElementById('Cover');
     image.src = imageUri;
 }
 
@@ -49,10 +59,14 @@ document.getElementById("cameraGetPicture").addEventListener("click", function()
     });
 });
      function onSuccessg(imageURL) {
-        var image = document.getElementById('myImage');
+        Title.textContent="réussi";
+        var image = document.getElementById('Cover');
         image.src = imageURL;
      }
   
      function onFailg(message) {
+        Title.textContent="echec";
         alert('Failed because: ' + message);
      }
+
+
